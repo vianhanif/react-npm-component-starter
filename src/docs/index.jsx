c43138dd469c1ee0caf830c63b1c51b2b86bfe7b
Alvian Rahman Hanif <alvian524@gmail.com>
2018-04-18 14:02:11 +0700

    clear samples

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import Meta from '../lib/components/Meta'
 import "./styles.css";
 
 function Demo() {
   return (
     <div>
-      <Meta title="Title">
-        <div>
-        </div>
-      </Meta>
+      <div>
+        Hello World
+      </div>
     </div>
   );
 }
